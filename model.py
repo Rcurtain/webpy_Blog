e4af0e67f36b2978d9bd2010b33f1d56bf54fad2
Rcurtain <r_curtain@163.com>
2018-01-11 16:28:09 +0800

    代码格式化

--- model.py
+++ model.py
@@ -4,22 +4,32 @@
 # model.py: Version 1
 import web, datetime
 
-db = web.database(dbn = 'mysql', db = 'guest', user = 'guest', pw = 'guest')
+db = web.database(dbn='mysql', db='guest', user='root', pw='#graylove12')
+
 
 def get_posts():
-	return db.select('entries', order = 'id DESC')
+    return db.select('entries', order='id DESC')
+
 
 def get_post(id):
-	try:
-		return db.select('entries', where = 'id=$id', vars=locals())[0]
-	except IndexError:
-		raise None
+    try:
+        return db.select('entries', where='id=$id', vars=locals())[0]
+    except IndexError:
+        raise None
+
 
 def new_post(title, text):
-	db.insert('entries', title = title, content = text, posted_on = datetime.datetime.utcnow())
+    db.insert(
+        'entries',
+        title=title,
+        content=text,
+        posted_on=datetime.datetime.utcnow())
+
 
 def del_post(id):
-	db.delete('entries', where = 'id = $id', vars = locals())
+    db.delete('entries', where='id = $id', vars=locals())
+
 
 def update_post(id, title, text):
-	db.update('entries', where = 'id = $id', vars = locals(), title = title, content = text)
+    db.update(
+        'entries', where='id = $id', vars=locals(), title=title, content=text)
